baf5d3cb3d61a26dad212a63c29481eabcb9b3c7
monsterxcn <monsterxcn@gmail.com>
2021-11-08 16:56:48 +0800

    添加 Artalk v1 评论系统组件

---
+++ src/components/ArtalkOld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="Comments"></div>
+</template>
+
+<script>
+import "artalk-v1/dist/Artalk.css"
+
+export default {
+  async mounted() {
+    const Artalk = require('artalk-v1')
+    var artalk = new Artalk({
+      el:           '#Comments',
+      pageKey:      'https://artalk-in-gridsome.vercel.app/about/',
+      // pageTitle: 'gridsome-about',
+      serverUrl:    'https://lab.monsterx.cn/ArtalkServer',
+      // site:      'artalk-in-gridsome',
+    })
+  }
+}
+</script>
+
+<style scoped>
+#Comments {
+  display: block;
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #F6F7F8;
+  box-shadow: 0 10px 20px rgba(0,0,0,.2);
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,11 +2,18 @@
   <Layout>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+
+    <ArtalkOld />
+
   </Layout>
 </template>
 
 <script>
+import ArtalkOld from '~/components/ArtalkOld'
 export default {
+  components: {
+    ArtalkOld,
+  },
   metaInfo: {
     title: 'About us'
   }
